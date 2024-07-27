2b95b36435b6ef91e71acc96278adbbd91e99de1
Md-abdul <mdabdulq62@gmail.com>
2024-07-27 12:58:04 +0530

    change home routes

--- backend/index.js
+++ backend/index.js
@@ -7,22 +7,19 @@ const { route } = require("./router/task.route");
 const router = require("./router/task.route");
 const taskModel = require("./model/task.model");
 const adminroutes = require("./router/admin.route");
-const port = 5050
+const port = 5050;
 app.use(cors());
 app.use(express.json());
 
-
 require("dotenv").config();
-
-app.use("/", (req, res) => {
-  res.json({ msg: "welcom to task manager backend" });
-});
 
 app.use("/admin", adminroutes);
 app.use("/users", userRouter);
 app.use("/task", router);
 
-
+app.get("/", (req, res) => {
+  res.send("welcome to task manager backend");
+});
 
 app.listen(port || 5000, async () => {
   try {
